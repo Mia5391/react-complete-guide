a7005056bf833952b76c71d731f5fa7c985afd39
Mia5391 <mariakrasteva5391@gmail.com>
2020-03-15 20:44:26 -0300

    basic lists (mapping array to JSX)

--- src/App.js
+++ src/App.js
@@ -52,18 +52,11 @@ class App extends Component {
         if (this.state.showPersons) {
             persons = (
                 <div>
-                    <Person
-                        name={this.state.persons[0].name}
-                        age={this.state.persons[0].age} />
-                    <Person
-                        name={this.state.persons[1].name}
-                        age={this.state.persons[1].age}
-                        click={this.switchNameHandler.bind(this, 'NewMiaClick')}
-                        changed={this.nameChangeHandler}>My hobbies: Racing
-                </Person>
-                    <Person
-                        name={this.state.persons[2].name}
-                        age={this.state.persons[2].age} />
+                    {this.state.persons.map(person =>{
+                        return <Person 
+                        name={person.name}
+                        age= {person.age} />
+                    })}
                 </div>
             );
         }
@@ -75,8 +68,8 @@ class App extends Component {
                     style={style}
                     onClick={() => this.togglePersonsHandler()}>Toggle cards
                     </button>
+                
                 {persons}
-
 
             </div>
         );
