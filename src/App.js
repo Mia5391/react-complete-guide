859cb15df12fa799f375db36d802e2c108a10997
Mia5391 <mariakrasteva5391@gmail.com>
2020-03-15 20:58:35 -0300

    remove person cards with splice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import person from './Person/Person';
 
 class App extends Component {
     state = {
@@ -34,6 +35,12 @@ class App extends Component {
         });
     }
 
+    deletePersonHandler = (personIndex) => {
+        const persons = this.state.persons;
+        persons.splice(personIndex, 1);
+        this.setState({persons:persons});
+    }
+
     togglePersonsHandler = () => {
         const doesShow = this.state.showPersons;
         this.setState({ showPersons: !doesShow });
@@ -52,8 +59,9 @@ class App extends Component {
         if (this.state.showPersons) {
             persons = (
                 <div>
-                    {this.state.persons.map(person =>{
+                    {this.state.persons.map((person, index) =>{
                         return <Person 
+                        click={() => this.deletePersonHandler(index)}
                         name={person.name}
                         age= {person.age} />
                     })}
