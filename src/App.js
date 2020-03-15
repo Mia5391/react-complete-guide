389fd1b1c73568a65ed2be7f5d420a1d6a1bcdea
Mia5391 <mariakrasteva5391@gmail.com>
2020-03-15 19:01:28 -0300

    two-way binding (props value and onChange)

--- src/App.js
+++ src/App.js
@@ -16,8 +16,18 @@ class App extends Component {
         // DO NOT DO THIS: this.state.persons[0].name='Changed Name';
         this.setState({
             persons: [
-                { name: newName, age: 29 },
+                { name: 'Mia', age: 29 },
                 { name: 'Cosi', age: 32 },
+                { name: 'Bobby', age: 33 }
+            ]
+        });
+    }
+
+    nameChangeHandler = (event) => {
+        this.setState({
+            persons: [
+                { name: 'Mia', age: 29 },
+                { name: event.target.value, age: 32 },
                 { name: 'Bobby', age: 33 }
             ]
         });
@@ -28,16 +38,18 @@ class App extends Component {
             <h1> Hi, I'm still.. a person</h1>
             <p>And I love...french fries!</p>
             <button onClick={() => this.switchNameHandler('MiaButton')}>Switch name</button>
-            <Person 
-            name={this.state.persons[0].name} 
-            age={this.state.persons[0].age} />
-            <Person 
-            name={this.state.persons[1].name} 
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'NewMiaClick')}>My hobbies: Racing</Person>
-            <Person 
-            name={this.state.persons[2].name} 
-            age={this.state.persons[2].age}></Person>
+            <Person
+                name={this.state.persons[0].name}
+                age={this.state.persons[0].age} />
+            <Person
+                name={this.state.persons[1].name}
+                age={this.state.persons[1].age}
+                click={this.switchNameHandler.bind(this, 'NewMiaClick')}
+                changed={this.nameChangeHandler}>My hobbies: Racing
+            </Person>
+            <Person
+                name={this.state.persons[2].name}
+                age={this.state.persons[2].age}></Person>
         </div>
         );
         // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hi, I\'m a COSI!!!!'));
