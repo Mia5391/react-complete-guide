ef25bd615358f940e8f4e71cbcd054cecb6410b9
Mia5391 <mariakrasteva5391@gmail.com>
2020-03-22 18:20:04 -0300

    replacing Aux with React.Fragment (built-in aux)

--- src/components/PersonsList/Person/Person.js
+++ src/components/PersonsList/Person/Person.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import Aux from '../../../hoc/Aux';
+import React, { Component, Fragment } from 'react';
+
 import classes from './Person.css';
 
 class Person extends Component {
     render() {
         return (
-            <Aux>
+            <React.Fragment>
                 <p key="i1" onClick={this.props.click}>
                     I'm {this.props.name} and I am {this.props.age} years old!
         </p>,
@@ -14,7 +14,7 @@ class Person extends Component {
                     key="i3"
                     type="text"
                     onChange={this.props.changed}
-                    value={this.props.name} /></Aux>
+                    value={this.props.name} /></React.Fragment>
 
 
         );
